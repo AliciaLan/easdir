ec3b68f1bfd90f809fd7fa39e93a73397b0855f8
alicia.lan <alicia.lan@isen.yncrea.fr>
2021-12-12 12:03:53 +0100

    CSS de la page principale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'easdir-root',
   template: `
-    <h1>Bienvenue dans EasDir</h1>
-    <img src="../assets/logo.png">
-    <router-outlet></router-outlet>
+    <header>
+      <div>
+        <img id="logo" src="../assets/logo.png">
+        <div id="titre">
+          <h1>Bienvenue dans <span id="easdir">EasDir</span></h1>
+          <h3>Votre nouveau gestionnaire de fichier</h3>
+        </div>
+      </div>
+    </header>
+
+    <div id="contenu">
+      <router-outlet></router-outlet>
+    </div>
+
+    <footer>
+      <p class="text-footer">Site Web développé en Angular JS dans le cadre d'un projet d'étude.</p>
+      <p class="text-footer">Projet réalisé par LAN Alicia et MUSOLES Hugo</p>
+    </footer>
   `,
-  styles: [`
-    img {
-      width:150px;
-      height:150px;
-    }
-  `]
+  styles: [``]
 })
 export class AppComponent {
   title = 'EasDir';
