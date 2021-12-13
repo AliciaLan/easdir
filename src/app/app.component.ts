048c598bb4dd36297de5a22b856ff82ed23a3e75
alicia.lan <alicia.lan@isen.yncrea.fr>
2021-12-13 10:28:54 +0100

    Boutons Ajout des boutons d'ajout d'élément et de suppression de dossier. Ajout en CSS et HTML seulement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,20 @@ import { Component } from '@angular/core';
     </header>
 
     <div id="contenu">
-      <h2>Dossier parent</h2>
-      <div id="elements">
+      <div id="contenu-titre">
+        <h2>Dossier parent</h2>
+        <button id="button-suppr-dossier" type="button">Supprimer le dossier</button>
+      </div>
+
+      <div id="contenu-button">
+        <button id="button-add-dossier" type="button">Ajouter un dossier</button>
+        <button id="button-add-text" type="button">Ajouter un fichier texte</button>
+      </div>
+
+      <div id="contenu-elements">
         <article class="dossier">
           <img src="../assets/dossier.png">
-          <p>nomedfghjkljhgfdsdfghjnk</p>
+          <p>nomedfghjkljhgfdsjnk</p>
         </article>
         <article class="text">
           <img src="../assets/fichier.png">
